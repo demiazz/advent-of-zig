f80644c4225fd46c65ea4d5c2860f0fe75292486
Alexey Plutalov <demiazz.py@gmail.com>
2024-11-11 19:12:16 +0300

    add solution for 2015/01/01

--- native/solutions/2015/01/part_1.zig
+++ native/solutions/2015/01/part_1.zig
@@ -3,6 +3,30 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const AnyReader = std.io.AnyReader;
 
-pub fn solve(_: Allocator, _: AnyReader) ![]const u8 {
-    return "It works!";
+pub fn solve(allocator: Allocator, reader: AnyReader) ![]const u8 {
+    var floor: i32 = 0;
+
+    while (reader.readByte()) |byte| {
+        floor += switch (byte) {
+            '(' => 1,
+            ')' => -1,
+            else => 0,
+        };
+    } else |err| {
+        if (err == error.EndOfStream) {
+            return try std.fmt.allocPrint(allocator, "{d}", .{floor});
+        } else {
+            return err;
+        }
+    }
+}
+
+test "solution 2015/01/01" {
+    const input_file = @embedFile("./input.fixture");
+    var input = std.io.fixedBufferStream(input_file);
+
+    const answer = try solve(std.testing.allocator, input.reader().any());
+    defer std.testing.allocator.free(answer);
+
+    try std.testing.expectEqualStrings(answer, "232");
 }
